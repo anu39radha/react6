5f5528a4a91b848086f54c4693615de249acf7b4
avinasharyan614 <avinasharyan614@gmail.com>
2021-08-27 14:04:47 +0530

    f

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Person from "./Person/Person";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> hi i am react app</h1>
+      <p> this is work it</p>
+
+      <person />
+      <person />
+      <person />
+    </div>
+  );
+}
+
+export default App;
